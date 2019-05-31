15c3a9282d6e3d1ea4a8fff8fc6caf179d9a39d2
ceshiwww007 <wjunshu123@163.com>
2019-05-31 15:59:29 +0800

    ﬁrst commit

---
+++ scripts/test_login.py
@@ -0,0 +1,14 @@
+class TestLogin:
+    def test_01(self):
+        print("test01")
+        assert 1
+
+    def test_02(self):
+        print("test02")
+        assert 0
+
+    def test_03(self):
+        print("test03")
+        assert 1
+
+
